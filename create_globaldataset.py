dbc288bfd69c28090a3ff3e04e265931f58f1d71
Prajwal T R <prajwaltr93@gmail.com>
2020-10-07 21:52:31 +0530

    data augmentation and create seperate train test validation dataset

--- create_globaldataset.py
+++ create_globaldataset.py
@@ -66,24 +66,22 @@ def plotImages(ind, X_loc_img, X_env_img, X_last_img, X_diff_img):
     axs[3].set_title("X_diff_img")
     plt.savefig("mygraph"+ind+".png")
 
-def pickleDataset(dataset, ind):
+def pickleDataset(dataset, ind, flag):
+    FLAGS = {0 : 'train', 1 : 'validation', 2 : 'test'}
     #check if incomming dataset is validation set
-    out_path = global_dataset_path+"data_batch_"+str(ind)
-    fd = open(out_path,"wb")
+    out_path = global_dataset_path + FLAGS[flag] + "_data_batch_" + str(ind)
+    fd = open(out_path, "wb")
     #convert list to numpy array ie : compatiable with tensorflow data adapter
     dataset['sG_data'] = np.array(dataset['sG_data'])
     dataset['sG_labels'] = np.array(dataset['sG_labels'])
     pic.dump(dataset,fd)
-    print("dataset created at : ",out_path)
+    print("dataset created at : ", out_path)
     #clear contents of dataset structure
     dataset['sG_data'] = []
     dataset['sG_labels'] = []
 
-if __name__ == "__main__":
-    sample_rate = int(sys.argv[1]) if (len(sys.argv) == 2) else 300 # accepts only integer arguments
-    #main loop
-    _, _, filelist = next(walk(traverse_path))
-    filelist = sorted(filelist)
+def createDataset(filelist, flag):
+    sample_rate = 300
     breaks = [i for i in range(0, len(filelist), sample_rate)]
     for break_ind in range(len(breaks) - 1):
         for file in filelist[breaks[break_ind] : breaks[break_ind + 1]]:
@@ -101,18 +99,28 @@ if __name__ == "__main__":
             label = Point()
             label.updatePoint(X_target[0])
             #creating images and labels
-            for index, m_index in zip(range(len(m_indices)),m_indices.__iter__()):
+            for index, m_index in zip(range(len(m_indices)), m_indices.__iter__()):
                 #each function call generates corresponding image
                 X_loc_img = drawPoint(X_loc)
                 X_env_img = drawStroke(X_env)
                 X_last_img = drawStroke(X_last)
                 X_diff_img = drawStroke(X_diff)
                 X_label_img = drawPoint(label)
-                #plotImages(str(index), X_loc_img, X_env_img, X_last_img, X_diff_img)
-                #update to dataset
-                dataset['sG_data'].append(np.dstack((X_loc_img, X_env_img, X_last_img, X_diff_img)))
-                dataset['sG_labels'].append(np.reshape(X_label_img, (HEIGHT * WIDTH)))
-                #udpate variables
+                # plotImages(str(index), X_loc_img, X_env_img, X_last_img, X_diff_img)
+                # data augementation
+                if flag == TRAIN:
+                    shift_x = ImageGen(width_shift = [-3, 3, 2])
+                    shift_x.flow([X_loc_img, X_env_img, X_last_img, X_diff_img, X_label_img])
+                    for X_loc_img, X_env_img, X_last_img, X_diff_img, X_label_img in shift_x:
+                        #update to dataset
+                        dataset['sG_data'].append(np.dstack((X_loc_img, X_env_img, X_last_img, X_diff_img)))
+                        dataset['sG_labels'].append(np.reshape(X_label_img, (HEIGHT * WIDTH)))
+                    # end of data augementation
+                if flag == TEST or flag == VALIDATION:
+                    #update to dataset
+                    dataset['sG_data'].append(np.dstack((X_loc_img, X_env_img, X_last_img, X_diff_img)))
+                    dataset['sG_labels'].append(np.reshape(X_label_img, (HEIGHT * WIDTH)))
+                # udpate variables
                 if (len(m_indices) == 1) or (index + 1 == len(m_indices)):
                     #X_target has only one stroke
                     continue
@@ -121,4 +129,31 @@ if __name__ == "__main__":
                 X_last = X_target[m_indices[index] : m_indices[index + 1]]
                 X_diff = X_target[m_indices[index + 1]:]
                 label.updatePoint(X_target[m_indices[index + 1]])
-        pickleDataset(dataset, break_ind)
+        pickleDataset(dataset, break_ind, flag)
+
+def help():
+    print(\
+    'USAGE: \n$python create_globaldataset.py train validation test\n\
+    \ntrain, validation, test - ratio to divide files')
+    exit(0)
+
+if __name__ == "__main__":
+    import sys
+    # argument processing
+    try:
+        train = int(sys.argv[1])
+        test = int(sys.argv[3])
+        validation = int(sys.argv[2])
+        if (train + validation + test) > 10 or (train + validation + test) < 0:
+            raise
+    except:
+        help()
+    _, _, filelist = next(walk(traverse_path))
+    filelist = sorted(filelist)
+    ratio = int(len(filelist) // (train + validation + test))
+    # train dataset
+    createDataset(filelist[ : train * ratio], TRAIN)
+    # validation datset
+    createDataset(filelist[train * ratio : validation * ratio], VALIDATION)
+    # test dataset
+    createDataset(filelist[validation * ratio : ], TEST)
